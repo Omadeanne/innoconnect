09267a4cfd71044f652db7c2dad82462cdd8069c
Lizkwong <elizabethokwong@gmail.com>
2023-11-22 11:39:26 +0100

    first dashboard commit

--- INNOCONNECT-React-Architecture/src/App.js
+++ INNOCONNECT-React-Architecture/src/App.js
@@ -7,13 +7,7 @@ import Community from './pages/community/Community';
 import JobSearch from './pages/job-search/JobSearch';
 import JobDetails from './pages/job-search/JobDetails';
 import Application from './pages/job-search/Application';
-import MentorForm from './pages/become-mentor/MentorForm';
-import Article from './pages/articles/Article';
-import Faq from './pages/articles/Faq';
 import Smartmatch from './pages/find-a-mentor/Smartmatch';
-import Pricing from './pages/pricing/Pricing';
-import About from './pages/about-us/About';
-import ForgotPassword from './pages/landing-page/ForgotPassword';
 
 
 function App() {
@@ -38,14 +32,7 @@ function App() {
       <Route path="/job-search" element={<JobSearch />} />
       <Route path='/job-details/:id' element={<JobDetails />} />
       <Route path="/apply/:id" element={<Application />} />
-      <Route path="/mentor-form" element={<MentorForm />} />
-      <Route path="/blog" element={<Article />} />
-      <Route path='/blog-details/:id' element={<JobDetails />} />  
-      <Route path="/faq" element={<Faq />} />
-      <Route path="/Smartmatch" element={<Smartmatch />} />
-      <Route path="/pricing" element={<Pricing />} />
-      <Route path="/about" element={<About />} />
-      <Route path="/forgot-password" element={<ForgotPassword />} />
+      <Route path="/Smart-match/" element={<Smartmatch />} />
       <Route path="*" element={<NotFound />} />
     </Routes>
   );
