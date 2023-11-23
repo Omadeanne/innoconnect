aede4d05ca1a4c917056200454411ed53209cdce
nnodim <nnodimobinna@gmail.com>
2023-11-23 10:49:10 +0100

    fixed bug and mentor form and mentee dashboard

--- INNOCONNECT-React-Architecture/src/pages/become-mentor/Submit.jsx
+++ INNOCONNECT-React-Architecture/src/pages/become-mentor/Submit.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 
-const Submit = ({ data }) => {
+const Submit = ({ data, back }) => {
   const listItems = Object.entries(data).map(([key, value]) => (
     <li className='sm:col-span-3'>
       <label
@@ -24,20 +24,28 @@ const Submit = ({ data }) => {
   return (
     <div className='mt-10'>
       <div className='px-5 py-2 bg-primary-04 mt-12 rounded-lg'>
-        <p className='text-white text-center'>
-          Review your Application
-        </p>
+        <p className='text-white text-center'>Review your Application</p>
       </div>
       <ul className='my-10 grid grid-cols-1 gap-x-6 gap-y-8 sm:grid-cols-6'>
         {listItems}
       </ul>
-      <button
-      id='nextBtn'
-      type='submit'
-      className='text-white font-semibold w-32 border bg-secondary-06 border-secondary-06 text-center rounded-lg px-4 py-2 block active:bg-secondary-07 hover:shadow-btn'
-    >
-      Submit
-    </button>
+      <div className='flex justify-between mt-10'>
+        <button
+          onClick={back}
+          id='prevBtn'
+          type='button'
+          className='text-white font-semibold w-32 border bg-secondary-06 border-secondary-06 text-center rounded-lg px-4 py-2 block active:bg-secondary-07 hover:shadow-btn'
+        >
+          Previous
+        </button>
+        <button
+          id='nextBtn'
+          type='submit'
+          className='text-white font-semibold w-32 border bg-secondary-06 border-secondary-06 text-center rounded-lg px-4 py-2 block active:bg-secondary-07 hover:shadow-btn'
+        >
+          Submit
+        </button>
+      </div>
     </div>
   );
 };
