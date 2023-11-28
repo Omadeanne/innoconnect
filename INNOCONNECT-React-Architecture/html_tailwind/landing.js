60f609de7217e2ef21e56cd088798378091d5943
nnodim <nnodimobinna@gmail.com>
2023-11-28 14:02:26 +0100

    Revert "images fixed"
    This reverts commit 2b5ac253612dacb0d7c1ce07843b9c6647a5adcb.

--- INNOCONNECT-React-Architecture/html_tailwind/landing.js
+++
@@ -1,5 +0,0 @@
-document.addEventListener("DOMContentLoaded", function() {
-  let ul = document.querySelector('[x-ref="logos"]');
-  ul.insertAdjacentHTML('afterend', ul.outerHTML);
-  ul.nextSibling.setAttribute('aria-hidden', 'true');
-});
