f1f5cdb8c5b0046064815e69ac72bfb8b7f73d81
nnodim <nnodimobinna@gmail.com>
2023-12-19 12:52:40 +0100

    emailverification

--- INNOCONNECT-React-Architecture/src/axios/axios.js
+++ INNOCONNECT-React-Architecture/src/axios/axios.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 const local = 'http://localhost:5000/v1';
 const remote = 'https://innoconnect-backend.onrender.com/v1';
 
-const BASE_URL = local;
+const BASE_URL = remote;
 
 export default axios.create({
   baseURL: BASE_URL,
